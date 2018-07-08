d06794e9852f22ded158f7b281a799bcccd403f9
shuxinpei <1025565665@qq.com>
2018-07-08 21:35:19 +0800

    The activity has finished

--- SplashLoginSignup-master/SplashLoginSignup-master/app/src/main/java/com/md/View/MainActivity.java
+++ SplashLoginSignup-master/SplashLoginSignup-master/app/src/main/java/com/md/View/MainActivity.java
@@ -1,17 +1,45 @@
 package com.md.View;
 
-import android.os.Bundle;
+
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+
 import com.md.splashloginsignup.R;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button bodydatast;
+    private Button history;
+    private Button count;
+    private Button logout;
+    private DrawerLayout drawerLayout;
+    private FragmentManager fm;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,10 +62,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        bodydatast = (Button)findViewById(R.id.bodydataset);
+        history = (Button)findViewById(R.id.history);
+        count = (Button)findViewById(R.id.count);
+        logout = (Button)findViewById(R.id.logout);
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm = getSupportFragmentManager();
+    }
+
+    public void  bodydatast(){
+        FragmentTransaction bt = fm.beginTransaction();
+        bt.replace(R.id.content, new Fragment());
+        bt.commit();
+    }
+    public void history(){
+        FragmentTransaction bt = fm.beginTransaction();
+        bt.replace(R.id.content, new Fragment());
+        bt.commit();
+    }
+    public void count(){
+        FragmentTransaction bt = fm.beginTransaction();
+        bt.replace(R.id.content, new Fragment());
+        bt.commit();
+    }
+    public void logout(){
+        startActivity(new Intent(this, SplashActivity.class));
     }
 
 }
