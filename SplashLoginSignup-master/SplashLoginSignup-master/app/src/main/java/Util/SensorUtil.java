0d03cee750cf34d70372aaf62e84c1f4d05753fa
shuxinpei <1025565665@qq.com>
2018-07-21 10:36:06 +0800

    2018-7-21

--- SplashLoginSignup-master/SplashLoginSignup-master/app/src/main/java/Util/SensorUtil.java
+++ SplashLoginSignup-master/SplashLoginSignup-master/app/src/main/java/Util/SensorUtil.java
@@ -6,13 +6,14 @@ import java.util.Map;
 public class SensorUtil {
     public static Map<String ,String> sensors = new HashMap<>();
     //这边返回传感器的状态
+    //这边需要用蓝牙
     public static Map<String ,String > getSensorSatus(){
         sensors.put("传感器1","连接");
-        sensors.put("传感器2","连接");
-        sensors.put("传感器3","连接");
-        sensors.put("传感器4","连接");
-        sensors.put("传感器5","连接");
-        sensors.put("传感器6","连接");
+        sensors.put("传感器2","未连接");
+        sensors.put("传感器3","未连接");
+        sensors.put("传感器4","未连接");
+        sensors.put("传感器5","未连接");
+        sensors.put("传感器6","未连接");
         return sensors;
     }
 }
